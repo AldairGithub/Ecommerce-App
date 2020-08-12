26a27744e277fa1da2de2b31b55d5cfa9eab2d02
acarneiro <acarneiro1413@gmail.com>
2020-08-12 17:07:57 -0400

    added display of items for each user

--- client/src/components/UserItems.jsx
+++ client/src/components/UserItems.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
-
 import { userItems } from '../services/users'
+
+import UserItemsCategories from './UserItemsCategories'
+import ItemData from './Item/ItemData'
 
 export default function UserItems(props) {
   
@@ -15,15 +17,17 @@ export default function UserItems(props) {
     const userList = await userItems(id)
     setUserItemsData(userList.items)
   }
-  console.log(userItemsData)
-
 
   return (
     <>
       <h1>This is where user items are displayed</h1>
       {userItemsData.map((item, index) => (
         <>
+          <img style={{ height: 100, width: 100 }} src={item.img_url} />
           <p key={index}>{item.name}</p>
+          <p>{item.price}</p>
+          <UserItemsCategories id={item.id}/>
+          {/* <ItemData item={item} /> */}
         </>
       ))}
     </>
