7dca23afef7db735c8beab1c02c88fbbce2bd477
acarneiro <acarneiro1413@gmail.com>
2021-05-28 00:45:39 -0400

    added css to categorized items in item data

--- client/src/components/Item/Item.jsx
+++ client/src/components/Item/Item.jsx
@@ -4,6 +4,7 @@ import Footer from '../footer/Footer'
 
 import ItemData from './item_data/ItemData'
 import SupplierItems from './supplier_items/SupplierItems'
+import CategoryItems from './category_items/CategoryItems'
 
 export default function Item(props) {
   const { items, cart, setCart, currentUser } = props
@@ -32,12 +33,6 @@ export default function Item(props) {
     // moreItemsFromCategories(itemData)
     moreItemsFromCategories(itemData)
     setItem(itemData)
-    // IMPORTANT
-    // items coming from parent component are depreciated once on item component, this array is set to null when user reloads page.
-    // Why? because it is not being called from database on the component! This array will be set to null because state is refreshed,
-    // it needs to come directly from database
-    // Steps? need to create a method on database that allows for render and filter of items based on categories
-    // console.log(filterItemsForCategoryDisplay(itemData, items))  
   }
 
   const moreItemsFromCategories = async(item) => {
@@ -80,6 +75,9 @@ export default function Item(props) {
               </div>
               <div style={{marginTop: '50px'}}>
                 <SupplierItems items={ supplierItems }/>
+              </div>
+              <div style={{marginTop: '50px'}}>
+                <CategoryItems items={ categoryItems.filter((ele, index) => ele.id !== parseInt(id) && index <= 7)}/>
               </div>
             </>
           }
