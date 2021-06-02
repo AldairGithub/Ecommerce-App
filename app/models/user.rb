bd52ef0de0a74c59c680c33d0634058fa9b3d2c0
acarneiro <acarneiro1413@gmail.com>
2021-06-02 12:33:53 -0400

    remodeled sign up page, edited address validation on user sign up so address is not mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :address, presence: true
-  validates :password, length: { minimum: 6}
+  validates :address, presence: { allow_blank: true, allow_nil: true}
+  validates :password, presence: true, length: { minimum: 6}
 
 end
