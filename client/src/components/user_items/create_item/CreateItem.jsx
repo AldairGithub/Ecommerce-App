26915afb4e7e7b604e0e3117a3cc6525cfb172e1
acarneiro <acarneiro1413@gmail.com>
2021-06-18 15:54:35 -0400

    edited and added more css to create item component

--- client/src/components/user_items/create_item/CreateItem.jsx
+++ client/src/components/user_items/create_item/CreateItem.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import './CreateItem.css'
 import { postItem } from '../../../services/items'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { faFileImage } from '@fortawesome/free-solid-svg-icons'
+import { faBookMedical } from '@fortawesome/free-solid-svg-icons'
 
 export default function CreateItem(props) {
   const [itemData, setItemData] = useState({
@@ -17,6 +22,14 @@ export default function CreateItem(props) {
     })
   }
 
+  const onFileChange = (e) => {
+    e.persist()
+    setItemData({
+      ...itemData,
+      img_url: e.target.files[0]
+    })
+  }
+
   // const handleSubmit = async (e) => {
   //   e.preventDefault()
   //   const newItem = await postItem(itemData)
@@ -30,9 +43,68 @@ export default function CreateItem(props) {
 
   return (
     <>
-
       <div className='create-item-container'>
-    {/* <form onSubmit={handleSubmit}> */}
+        <form>
+          <div>
+            <div className='create-item-label-container'>
+              <label>What name would you describe your item by?</label>
+            </div>
+            <div className='create-item-input-container'>
+              <FontAwesomeIcon id='create-item-icon' icon={faEdit} size='1x' />
+              <input
+                className='create-item-input'
+                type='text'
+                name='name'
+                value={itemData.name}
+                onChange={handleChange}
+                placeholder='Name'
+              />
+            </div>
+          </div>
+          <div>
+            <div className='create-item-label-container'>
+              <label>What price would work for you?</label>
+            </div>
+            <div className='create-item-input-container'>
+              <FontAwesomeIcon id='create-item-icon' icon={faDollarSign} size='1x' />
+              <input
+                className='create-item-input'
+                type='number'
+                name='price'
+                value={itemData.price}
+                onChange={handleChange}
+                placeholder='Price'
+              />
+            </div>
+
+          </div>
+          <div>
+            <div className='create-item-label-container'>
+              <label>What image would best describe your product?</label>
+            </div>
+            <div className='create-item-input-file-container'>
+              <FontAwesomeIcon id='create-item-icon-file' icon={faFileImage} size='1x' />
+              <input
+                className='create-item-input-file'
+                type='file'
+                name='image'
+                accept='.png, .jpg, .jpeg'
+                onChange={(e) => onFileChange(e)}
+              />
+            </div>
+
+          </div>
+          <div className='create-item-button-container'>
+            <button className='create-item-button'>
+              Add to Catalog
+              <FontAwesomeIcon id='create-item-add-item-icon'  icon={ faBookMedical } size='1x'/>
+            </button>
+          </div>
+        </form>
+      </div>
+{/* 
+      <div className='create-item-container'>
+    <form onSubmit={handleSubmit}>
       <form>
       <input
         type='text'
@@ -57,7 +129,7 @@ export default function CreateItem(props) {
       />
       <button>Submit</button>
       </form>
-      </div>
+      </div> */}
     </>
   )
 }
